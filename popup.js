e5de160146f1edaf452ff50e5b8a3b6349c88fec
Elmira Tap <elmiratap@gmail.com>
2018-02-14 19:35:33 -0800

    Fixed bug allowing empty search query to run

--- popup.js
+++ popup.js
@@ -43,7 +43,9 @@ function getBookTitleAndAuthorFromAmazon(callback) {
 // Remove parentheticals and the part of the title after (and including) a colon
 function abbreviateTitle(fullTitle) {
   for (var i=0; i<fullTitle.length; i++) {
-    if (fullTitle.charAt(i) === ':' || fullTitle.charAt(i) == '(') {
+    if (fullTitle.charAt(i) === ':') {
+      return fullTitle.substring(0, i);
+    } else if (fullTitle.charAt(i) == '(') {
       return fullTitle.substring(0, i-1); // Remove extra space at the end
     }
   }
@@ -58,22 +60,25 @@ function searchGoodreads(tab) {
   // https://www.goodreads.com/search?q=The+Second+Chance+Cafe+alison+kent
   var queryTextbox = document.getElementById("goodreadsQueryTextbox");
   var query = queryTextbox.value;
+  console.log(query);
   if (query !== "") {
     console.log(query);
     goodreadsURL += query.replace(/ /g, '+');
-  }
-  if (tab === "current") {
-    chrome.tabs.update({url: goodreadsURL});
-  } else if (tab === "new") {
-    chrome.tabs.create({url: goodreadsURL});
+    if (tab === "current") {
+      chrome.tabs.update({url: goodreadsURL});
+    } else if (tab === "new") {
+      chrome.tabs.create({url: goodreadsURL});
+    }
+  } else {
+    console.log("query is empty");
   }
 }
-
 
 // This extension performs a search on Goodreads in two ways:
 // 1. Gathering the title and author of a book from the open Amazon page
 // 2. Allowing the user to enter a query of their own
 document.addEventListener('DOMContentLoaded', () => {
+  console.log(document.getElementById("goodreadsQueryTextbox").value);
   var currentTabButton = document.getElementById("currentTabButton");
   currentTabButton.addEventListener('click', function() {
     searchGoodreads("current");
@@ -87,12 +92,14 @@ document.addEventListener('DOMContentLoaded', () => {
   currentTabOnAmazon((onAmazon) => {
     if (onAmazon) {
       getBookTitleAndAuthorFromAmazon((titleAndAuthor) => {
-        var query = `${abbreviateTitle(titleAndAuthor.title)} ${titleAndAuthor.author}`;
-        console.log(query);
-        pasteQueryIntoTextbox(query);
+        if (titleAndAuthor !== null) {
+          var query = `${abbreviateTitle(titleAndAuthor.title)} ${titleAndAuthor.author}`;
+          console.log(query);
+          pasteQueryIntoTextbox(query);
+        } else {
+          console.log("title and author are null");
+        }
       });
-    } else {
-      // alert("This tab is NOT on Amazon!");
     }
   });
 });
